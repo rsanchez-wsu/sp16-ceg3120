7442aa7cc7ae8d93b95cd995c1fb4072eb637d67
iZezoh <Mr.Ziyad-2009@hotmail.com>
2016-02-04 21:35:27 -0500

    I added the first draft of Profile class that shows the profile window but nothing does anything. Issue #18

--- src/edu/wright/cs/sp16/ceg3120/MainApp.java
+++ src/edu/wright/cs/sp16/ceg3120/MainApp.java
@@ -21,12 +21,15 @@
 
 package edu.wright.cs.sp16.ceg3120;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ContainerEvent;
+import java.awt.event.ContainerListener;
+
 
 /**
  * The application's main class.
@@ -37,15 +40,32 @@ public class MainApp {
 	 * 
 	 * 
 	 */
-	private static void closeWindow() {
+	private static void createWindow() {
 		
 		final JFrame frame = new JFrame("Hello World");
 		JLabel label = new JLabel("Hello World");
+		JButton b1 = new JButton("Profile");
 		frame.getContentPane().add(label);
-		frame.addWindowListener(new WindowAdapter() {
-			
+		frame.getContentPane().add(b1);
+		b1.addActionListener(new ActionListener() {
+
 			@Override
-			public void windowClosing(WindowEvent we) {
+			public void actionPerformed(ActionEvent e) {
+			    if (e.getActionCommand().equals("Profile")) {
+			    	Profile x = new Profile();
+			    	System.out.println("Hi");
+			    }
+			    
+				
+			}
+			
+			
+		});
+		
+		
+		frame.addContainerListener(new ContainerListener() {
+			
+			public void windowClosing(Window we) {
 				int close = JOptionPane.showConfirmDialog(frame, 
 						"Exit the application?", 
 						"Exit", 
@@ -58,7 +78,23 @@ public class MainApp {
 					frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 				}
 			}
+
+			@Override
+			public void componentAdded(ContainerEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void componentRemoved(ContainerEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
 		});
+		
+		
 		
 		frame.setSize(300, 300);
 		frame.setLocationRelativeTo(null);
@@ -74,14 +110,16 @@ public class MainApp {
 	public static void main(String[] args) {
 		// Schedule a job for the event-dispatching thread:
 		// creating and showing this application's GUI.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				
 				
-				closeWindow();
-
+				createWindow();
 			}
 		});
+		
+
+		
 	}
 }
