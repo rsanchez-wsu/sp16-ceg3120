d07157a671d8e436a61c9b82f1d4dc7c5926384c
sam <samstevens.ohio@gmail.com>
2016-02-09 21:46:58 -0500

    #36 added classes for BackupExportTab and ConnectionTab, added call to ConnectionTab in MainGUi.java

---
+++ src/edu/wright/cs/sp16/ceg3120/gui/tabs/ConnectionTab.java
@@ -0,0 +1,21 @@
+package edu.wright.cs.sp16.ceg3120.gui.tabs;
+
+import java.awt.Component;
+
+public class ConnectionTab extends Component {
+	
+	public ConnectionTab(){
+		super();
+		
+		initComponents();
+	}
+	
+	/*
+	 * TODO: create all components for this window and
+	 * 		 initialize them here.
+	 * */
+	private void initComponents() {
+		
+		
+	}
+}
