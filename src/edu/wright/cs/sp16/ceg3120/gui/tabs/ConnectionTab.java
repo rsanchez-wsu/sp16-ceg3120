229d6abfad83b12a60de280fb1cad6ad296351ec
Cody Butz <codyjbutz@gmail.com>
2016-02-11 11:29:50 -0500

    Included commetns for BackupExportTab and ConnectionTab. Fixes build.

--- src/edu/wright/cs/sp16/ceg3120/gui/tabs/ConnectionTab.java
+++ src/edu/wright/cs/sp16/ceg3120/gui/tabs/ConnectionTab.java
@@ -1,16 +1,48 @@
+/*
+ * Copyright (C) 2016
+ * 
+ * 
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package edu.wright.cs.sp16.ceg3120.gui.tabs;
 
 import java.awt.Component;
 
+/**
+ * The tab that contains all the components for the connection.
+ * 
+ * @author codybutz
+ *
+ */
 public class ConnectionTab extends Component {
 	
-	public ConnectionTab(){
+	private static final long serialVersionUID = -9056641573923593935L;
+	
+	/**
+	 * Default constructor, initializes components.
+	 */
+	public ConnectionTab() {
 		super();
 		
 		initComponents();
 	}
 	
-	/*
+	/**
 	 * TODO: create all components for this window and
 	 * 		 initialize them here.
 	 * */
