6c43c25ee1a0cb6fc7eed7aa3a8a4f3d09eef602
Ali Guyton <AlisonGuyton@Alis-MacBook-Pro.local>
2016-02-11 17:23:30 -0500

    fix build attempt

--- src/edu/wright/cs/sp16/ceg3120/MainApp.java
+++ src/edu/wright/cs/sp16/ceg3120/MainApp.java
@@ -56,42 +56,14 @@ public class MainApp {
 	static void connectTodb() {
 		// JDBC driver name and database URL
 		JDBC_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-<<<<<<< HEAD
-		//DB_URL_BASE = "jdbc:derby:";//JBDC compliant DBMS: derby, mySQL, 
-		//  Database credentials
-		//String user = "";
-		//String password = "";
-		//Connection conn = null;
-		//Statement stmt = null;
 		try ( Connection conn = DriverManager.getConnection(DB_URL);
 				Statement stmt = conn.createStatement(); ) {
-			//STEP 2: Register JDBC driver
+			// Register JDBC driver
 			Class.forName(JDBC_DRIVER).newInstance();
 
-			//STEP 3: Open a connection
+			// Open a connection
 			System.out.println("Connecting to database...");
-			//conn = DriverManager.getConnection(DB_URL);
 
-//			//STEP 4: Execute a query
-//			System.out.println("Creating statement...");
-//			//stmt = conn.createStatement();
-//			String sql;
-//			sql = "SELECT * FROM TEAM6";
-//			ResultSet rs = stmt.executeQuery(sql);
-//
-//			//STEP 5: Extract data from result set
-//			int colCount = rs.getMetaData().getColumnCount();
-//			while (rs.next()) {
-//				String rowContent = new String("");
-//				for (int i = 1; i <= colCount; i++) {
-//					rowContent = rowContent + " : " + rs.getString(i);
-//				}
-//				System.out.println(rowContent);
-//			}
-//			//STEP 6: Clean-up environment
-//			rs.close();
-			//stmt.close();
-			//conn.close();
 		} catch (SQLException se) {
 			//Handle errors for JDBC
 			se.printStackTrace(); 
@@ -99,60 +71,19 @@ public class MainApp {
 			//Handle errors for Class.forName
 			e.printStackTrace();
 		} 
-//		finally {
-//			//finally block used to close resources
-//			try {
-//				if (stmt != null) {
-//					stmt.close();
-//				}
-//			} catch (SQLException se2) {
-//				System.out.println("Nothing we can do");
-//			} // nothing we can do
-//			try {
-//				if (conn != null) {
-//					conn.close();
-//				}
-//			} catch (SQLException se) {
-//				se.printStackTrace();
-//			} //end finally try
-//		} //end try
+
 		System.out.println("Goodbye!");
-=======
-		// DB_URL_BASE = "jdbc:derby:";//jdbc compliant DBMS: derby, mysql,
-
-
-		// Database credentials
-		// String user = "";
-		// String pass = "";
-
 
 		try (Connection conn = DriverManager.getConnection(DB_URL); 
 				Statement stmt = conn.createStatement()) {
 			try {
-				// STEP 2: Register JDBC driver
+				// Register JDBC driver
 				Class.forName(JDBC_DRIVER).newInstance();
 
-				// STEP 3: Open a connection
+				// Open a connection
 				System.out.println("Connecting to database...");
 
-				// STEP 4: Execute a query
-				System.out.println("Creating statement...");
-				String sql;
-				sql = "SELECT * FROM TEAM6";
-				ResultSet rs = stmt.executeQuery(sql);
-
-				// STEP 5: Extract data from result set
-				int colCount = rs.getMetaData().getColumnCount();
-				while (rs.next()) {
-					String rowContent = new String("");
-					for (int i = 1; i <= colCount; i++) {
-						rowContent = rowContent + " : " + rs.getString(i);
-					}
-					System.out.println(rowContent);
-				}
-
-				// STEP 6: Clean-up environment
-				rs.close();
+				// Clean-up environment
 				stmt.close();
 				conn.close();
 
@@ -162,27 +93,15 @@ public class MainApp {
 			} catch (Exception e) {
 				// Handle errors for Class.forName
 				e.printStackTrace();
-			} finally {
-				// finally block used to close resources
-				// try{
-				// if(stmt!=null)
-				// stmt.close();
-				// }catch(SQLException se2){
-				// }// nothing we can do
-				// try{
-				// if(conn!=null)
-				// conn.close();
-				// }catch(SQLException se){
-				// se.printStackTrace();
-				// }//end finally try
-			} // end try
+			} 
+				
 			System.out.println("Goodbye!");
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
->>>>>>> 159df2a9b01e6ba8bafeee48f56b6bca581c738c
+
 	}
 
 	/**
@@ -216,8 +135,7 @@ public class MainApp {
 		connectionWindow.getContentPane().add(passLabel);
 		connectionWindow.getContentPane().add(passTextField);
 		connectionWindow.getContentPane().add(submitConnButton);
-		// frame.getContentPane().add(userLabel);
-		// frame.getContentPane().add(userLabel);
+		
 
 		connectionWindow.addWindowListener(new WindowAdapter() {
 
