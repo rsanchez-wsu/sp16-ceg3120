956da691961715ac808a539fbe0a3e35ee05940c
Bonnie Shields <bon.shields@gmail.com>
2016-02-12 14:53:14 -0500

    Removed unneeded imports.
    Removed imports that were not being used. WindowEvent listener, etc.
    Issue #12

--- src/edu/wright/cs/sp16/ceg3120/MainFrame.java
+++ src/edu/wright/cs/sp16/ceg3120/MainFrame.java
@@ -24,14 +24,11 @@ package edu.wright.cs.sp16.ceg3120;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 
 /**
  * Main Frame for application.
