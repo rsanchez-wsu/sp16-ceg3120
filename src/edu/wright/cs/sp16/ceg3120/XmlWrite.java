60de0ddcd72c94f9850d36ed4ee39872f703e31d
meleno10 <meganlnorris@hotmail.com>
2016-02-13 21:59:33 -0500

    Edited XMLWrite to fix method name issue.
    XMLWrite contained a main method; this was changed into a method that is
    passed a string and returns void. Also some extraneous lines of code
    were removed and an import issue was fixed.
    Issue #37

--- src/edu/wright/cs/sp16/ceg3120/XmlWrite.java
+++ src/edu/wright/cs/sp16/ceg3120/XmlWrite.java
@@ -32,6 +32,7 @@ import java.io.StringWriter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -41,36 +42,34 @@ import javax.xml.transform.stream.StreamResult;
 
 /**
  * Class that parses xml and should store it in a document.
- * Tyrone, I hope this is what you meant by this but all the errors are fixed.
+ *
  *
  */
 public class XmlWrite {
 	/**
-	 * Unfinished method that really shouldn't be main, Tyrone, what's going on here 
-	 * and what's supposed to be going on?
-	 * @param args standard (but shouldn't exist because this shouldn't be main
+	 * Unfinished method that will parse xml and save user preferences into Profile.xml
+	 * @param filename name of file for parsing xml information. Also stores the xml data (string)
 	 * @throws TransformerException exception handling for creating Transformer
 	 * @throws ParserConfigurationException exception handling for creating newDocumentBuilder
 	 * @throws SAXException exception handling for using docBuilder.parse
 	 * @throws IOException exception handling for using docBuilder.parse
 	 */
-	public static void main(String[] args) throws TransformerException, 
+	public static void writeXml(String filename) throws TransformerException, 
 	ParserConfigurationException, SAXException, IOException {
 		
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-		//Is profile.xml the correct thing to insert in this statement?
-		Document doc = docBuilder.parse("Profile.xml");//enter input source inside the brackets
+		//I added a string passed to this method as the file
+		Document doc = docBuilder.parse(filename);//enter input source inside the brackets
 		//this is where we can add attributes and tags
 		//write xml document to a string
 		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-	//	transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 		StreamResult result = new StreamResult(new StringWriter());
 		DOMSource source = new DOMSource(doc);
 		transformer.transform(source,result);
 				
 		String xmlString = result.getWriter().toString();
-		System.out.println(xmlString);
 		System.out.println(xmlString);
 	}
 }
