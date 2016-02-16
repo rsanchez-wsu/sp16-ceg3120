17ccf094a40f460dc55533da9841aa549b3dc760
iZezoh <Mr.Ziyad-2009@hotmail.com>
2016-02-15 20:00:48 -0500

    Returned the main method for Profile.java.

--- src/edu/wright/cs/sp16/ceg3120/Profile.java
+++ src/edu/wright/cs/sp16/ceg3120/Profile.java
@@ -360,6 +360,49 @@ public class Profile extends JFrame {
 		pack();
 	} // End of initComponents method ,,,
 
+	/**
+	 * @param args
+	 *            the command line arguments.
+	 */
+	public static void main(String[] args) {
+		/* Set the Nimbus look and feel */
+		// <editor-fold default state="collapsed" desc=" Look and feel setting
+		// code (optional) ">
+		/*
+		 * If Nimbus (introduced in Java SE 6) is not available, stay with the
+		 * default look and feel. For details see
+		 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.
+		 * html
+		 */
+		try {
+			for (UIManager.LookAndFeelInfo info : UIManager
+					.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(Profile.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(Profile.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(Profile.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(Profile.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		}
+		// </editor-fold>
+
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Profile().setVisible(true);
+			}
+		});
+	}
+
 } // End of Profile class ,,,
-
-
