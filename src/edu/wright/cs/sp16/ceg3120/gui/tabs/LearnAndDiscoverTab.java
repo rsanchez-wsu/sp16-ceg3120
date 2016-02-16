0ea9012294dd7f431a6d3ef467027d87a6bacb07
Alex Vance <AlexVance17@Gmail.com>
2016-02-15 21:52:28 -0500

    Fleshed out event driven buttons for hyperlinks

--- src/edu/wright/cs/sp16/ceg3120/gui/tabs/LearnAndDiscoverTab.java
+++ src/edu/wright/cs/sp16/ceg3120/gui/tabs/LearnAndDiscoverTab.java
@@ -21,6 +21,13 @@
 
 package edu.wright.cs.sp16.ceg3120.gui.tabs;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 //import javax.swing.JTabbedPane;
 
 /**Learn and Discover tab, providing resources for helpful info lookup.
@@ -46,6 +53,60 @@ public class LearnAndDiscoverTab {
 	private void initComponents() {
 		//JTabbedPane pane = new JTabbedPane();
 		
+		//URL information for tool tips
+		final String sqlUrl = "http://www.w3schools.com/sql/";
+		final String driverInfoUrl = "";
+		final String jenkinsUrl = "https://jenkins-ci.org/";
+		final String derbyUrl = "https://db.apache.org/derby/";
+		final String githubUrl = "https://github.com/rsanchez-wsu/sp16-ceg3120";
+		
+		JFrame frame = new JFrame("Links");
+		
+		final JButton sqlButton = new JButton();
+		final JButton driverButton = new JButton();
+		final JButton jenkinsButton = new JButton();
+		final JButton derbyButton = new JButton();
+		final JButton gitButton = new JButton();
+		
+		Container container = frame.getContentPane();
+		container.setLayout(new GridBagLayout());
+		
+		sqlButton.setText("SQL help/reference: ");
+		sqlButton.setOpaque(false);
+		sqlButton.setBackground(Color.WHITE);
+		sqlButton.setToolTipText(sqlUrl);
+		//sqlButton.addActionListener(new OpenUrlAction());
+		container.add(sqlButton);
+		
+		driverButton.setText("Driver information: ");
+		driverButton.setOpaque(false);
+		driverButton.setBackground(Color.WHITE);
+		driverButton.setToolTipText(driverInfoUrl);
+		//driverButton.addActionListener(new OpenUrlAction());
+		container.add(driverButton);
+		
+		jenkinsButton.setText("Jenkins: ");
+		jenkinsButton.setOpaque(false);
+		jenkinsButton.setBackground(Color.WHITE);
+		jenkinsButton.setToolTipText(jenkinsUrl);
+		//jenkinsButton.addActionListener(new OpenUrlAction());
+		container.add(jenkinsButton);
+		
+		derbyButton.setText("Derby: ");
+		derbyButton.setOpaque(false);
+		derbyButton.setBackground(Color.WHITE);
+		derbyButton.setToolTipText(derbyUrl);
+		//derbyButton.addActionListener(new OpenUrlAction());
+		container.add(derbyButton);
+		
+		gitButton.setText("Github: ");
+		gitButton.setOpaque(false);
+		gitButton.setBackground(Color.WHITE);
+		gitButton.setToolTipText(githubUrl);
+		//driverButton.addActionListener(new OpenUrlAction());
+		container.add(gitButton);
+				
+		//TODO: add links to preferences page as well as help within app
+		
 	}
-
 }
