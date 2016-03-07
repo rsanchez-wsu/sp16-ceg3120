6b485b611dc117905c648eba8335ac76529975e7
Zach Cole <cole.134@wright.edu>
2016-03-07 16:00:29 -0500

    Basic implementation to at least execute a statement and get a result, or fail.

--- src/edu/wright/cs/sp16/ceg3120/SqlStatement.java
+++ src/edu/wright/cs/sp16/ceg3120/SqlStatement.java
@@ -1,5 +1,8 @@
 package edu.wright.cs.sp16.ceg3120;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 /**
  * This class will be designed to handle SQL queries passed to it and to provide a result set gained 
  * from the query.
@@ -7,20 +10,38 @@ package edu.wright.cs.sp16.ceg3120;
 
 import java.sql.Statement;
 import java.util.Scanner;
-
+/*
+ * This class will take SQL statements and produce appropriate result sets or throw an
+ * exception
+ */
 public class SqlStatement {
 	
-	//default constructor
-	public SqlStatement()
-	{
-		Statement stat = null;
-		String query = null;
-	}
+	Connection conn;
+	Statement stat = null;
+	String query;
+	
 	//constructor built to take a SQL input
-	public SqlStatement(String enteredQuery)
+	public SqlStatement(Connection conn, String enteredQuery)
 	{
-		Statement stat = null;
-		String query = enteredQuery;
+		this.conn = conn;
+		this.query = enteredQuery;
+	}
+	
+	/*
+	 * This class will determine if an statement is executable.
+	 */
+	public void executeStat()
+	{
+		String success = "Statement executed.";
+		String failed = "Statement failed to execute";
+		
+		try
+		{
+			stat = conn.createStatement();
+			ResultSet rs = conn.;
+			return rs;
+		};
+		
 	}
 	
 	
