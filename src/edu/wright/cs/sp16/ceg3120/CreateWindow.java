e7cad085b16a353f9b46c071c47927f21775ff59
nickmadden <madden.25@wright.edu>
2016-03-12 14:56:11 -0500

    Updated GUI too be more user friendly.
    Changed text options of GUI. GUI layout updated to be easier to read. No
    longer will attempt to load "Choose an alias" when selected.
    #112

--- src/edu/wright/cs/sp16/ceg3120/CreateWindow.java
+++ src/edu/wright/cs/sp16/ceg3120/CreateWindow.java
@@ -22,12 +22,15 @@
 package edu.wright.cs.sp16.ceg3120;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.SQLException;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -38,6 +41,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 /**
  * @author Devesh Amin, Nick Madden.
@@ -47,23 +52,13 @@ public class CreateWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	// title label
-	private JLabel title = new JLabel("Aliases");
-
-	// new buttons
-	private JButton clear = new JButton("Clear");
-	private JButton connect = new JButton("Connect");
-
 	// input fields
 	public static JComponent[] inputFields = new JComponent[8];
-	static String[] driverNames = { "None Selected",
+	static String[] driverNames = { "Choose a Driver",
 			"MySQL Driver", "PostgreSQL Driver", "Demo Driver 3" };
 	private static JComboBox<String> aliases;
 
 	// creating panels to add labels and text boxes
-	private JPanel titlePanel = new JPanel();
-	private JPanel controlPanel = new JPanel();
-	private JPanel buttonPanel = new JPanel();
 	private static JPanel svAlias = new JPanel();
 
 	// ActionListener for buttons
@@ -76,25 +71,29 @@ public class CreateWindow extends JFrame {
 		super("Connect to Database");
 
 		// Title Panel and its position
-		createTitlePanel(title);
-		createControlPanel();
-		getContentPane().add(titlePanel, BorderLayout.NORTH);
-
+		getContentPane().add(createTitlePanel(), BorderLayout.NORTH);
+		
 		// Create gui input fields
 		JPanel gui = new JPanel();
 		gui.setLayout(new GridLayout(8, 2, 30, 10));
 
 		for (int i = 0; i <= 7; i++) {
 			JPanel jpanel = new JPanel();
-			jpanel.setLayout(new GridLayout(0, 2, 10, 0));
-			jpanel.add(new JLabel(Inputs.get(i).toString()));
+			JLabel jlabel = new JLabel(Inputs.get(i).toString());
+			jlabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+			jpanel.setLayout(new GridLayout(0, 2, 10, 20));
+			jpanel.add(jlabel);
 
 			if (i == Inputs.driver.getId()) {
+				jlabel.setText("");
 				JComboBox<String> jcb = new JComboBox<String>(driverNames);
+				jcb.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
 				inputFields[i] = jcb;
 				jpanel.add(jcb);
 			} else if (i == Inputs.save.getId() || i == Inputs.autoCon.getId()) {
+				jlabel.setText("");
 				JCheckBox jcb = new JCheckBox();
+				jcb.setText((i == Inputs.save.getId() ? "Save Password" : "Auto Connect"));
 				inputFields[i] = jcb;
 				jpanel.add(jcb);
 			} else if (i == Inputs.password.getId()) {
@@ -111,37 +110,47 @@ public class CreateWindow extends JFrame {
 		
 		// Add to panels
 		getContentPane().add(gui, BorderLayout.CENTER);
-		getContentPane().add(controlPanel, BorderLayout.SOUTH);
+		getContentPane().add(createControlPanel(), BorderLayout.SOUTH);
 	}
 
 	/** Adding title to panel.
 	 * @author Devesh Amin, Nick Madden
 	 */
-	private void createTitlePanel(JLabel tt) {
+	private JPanel createTitlePanel() {
 		// Get list of saved aliases for combobox
+		JPanel jpanel = new JPanel();
 		String[] listA = XmlHandler.populateAlias();
-		
-		titlePanel.setLayout(new GridLayout(2, 1));
-		titlePanel.add(tt);
 		aliases = new JComboBox<String>(listA);
+		
+		JLabel title = new JLabel("Create an Alias", SwingConstants.CENTER);
+		jpanel.setLayout(new GridLayout(3, 1));
+		jpanel.add(title);
+		
 		aliases.setActionCommand("Alias");
+		aliases.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
 		aliases.addActionListener(actionHandler);
-		titlePanel.add(aliases);
+		jpanel.add(aliases);
+		return jpanel;
 	}
 
 	/** Adds buttons to window.
 	 * @author Devesh Amin, Nick Madden
 	 */
-	private void createControlPanel() {
-		controlPanel.setLayout(new GridLayout(1, 2));
-		buttonPanel.setLayout(new GridLayout(1, 2));
-		buttonPanel.add(connect);
-		buttonPanel.add(clear);
-		clear.setActionCommand("Clear");
-		clear.addActionListener(actionHandler);
+	private JPanel createControlPanel() {
+		JPanel jpanel = new JPanel();
+		jpanel.setLayout(new FlowLayout(FlowLayout.TRAILING, 10, 20));
+		
+		JButton connect = new JButton("Connect");
 		connect.setActionCommand("Connect");
 		connect.addActionListener(actionHandler);
-		controlPanel.add(buttonPanel);
+		jpanel.add(connect);
+		
+		JButton clear = new JButton("Clear");
+		clear.setActionCommand("Clear");
+		clear.addActionListener(actionHandler);
+		jpanel.add(clear);
+
+		return jpanel;
 	}
 
 	/** Save alias to file.
@@ -159,9 +168,10 @@ public class CreateWindow extends JFrame {
 		boolean svPass = ((JCheckBox) inputFields[Inputs.save.getId()]).isSelected();
 		//Autoconnect not yet a feature.
 		//boolean autoCon = ((JCheckBox) inputFields[Inputs.autoCon.getId()]).isSelected();
-		
-		int sv = JOptionPane.showConfirmDialog(svAlias, "Do you want to save "
-				+ alias + " alias?", "Save Alias?", JOptionPane.YES_NO_CANCEL_OPTION);
+		UIManager.put("OptionPane.yesButtonText", "Save and Connect");
+		UIManager.put("OptionPane.noButtonText", "Connect");
+		int sv = JOptionPane.showConfirmDialog(svAlias, "Do you want to save the \""
+				+ alias + "\" alias?", "Save Alias?", JOptionPane.YES_NO_CANCEL_OPTION);
 		if (sv == JOptionPane.YES_OPTION) {
 			String pass = "";
 			if (svPass) {
@@ -174,6 +184,9 @@ public class CreateWindow extends JFrame {
 		} else if (sv == JOptionPane.CANCEL_OPTION) {
 			((JTextField) inputFields[Inputs.alias.getId()]).grabFocus();
 		}
+		
+		UIManager.put("OptionPane.yesButtonText", "Yes");
+		UIManager.put("OptionPane.noButtonText", "No");
 		
 		switch (dbDriver) {
 		// MySQL Driver
@@ -261,9 +274,11 @@ public class CreateWindow extends JFrame {
 				
 			case "Alias":
 				String toRead = aliases.getSelectedItem().toString();
-				((JTextField) inputFields[Inputs.alias.getId()]).setText(toRead);
-				((JTextField) inputFields[Inputs.alias.getId()]).grabFocus();
-				XmlHandler.readAlias(toRead, inputFields);
+				if (!toRead.equals("Load an Alias")) {
+					((JTextField) inputFields[Inputs.alias.getId()]).setText(toRead);
+					((JTextField) inputFields[Inputs.alias.getId()]).grabFocus();
+					XmlHandler.readAlias(toRead, inputFields);
+				}
 				break;
 				
 			default: System.out.println("ERROR: Action not found!");
