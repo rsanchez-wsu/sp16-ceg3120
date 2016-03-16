4cb7e2f9034db646f242544adb840af717ccc7a4
Bonnie Shields <bon.shields@gmail.com>
2016-03-16 08:13:02 -0400

    Created ConnectionTabPane & QueryBuilderTab class
    Working on setting up a QueryBuilder tab underneath the Connection tab
    in the main GUI. Setting this up similar to the StartPage and Learn &
    Discover tab.
    Work in Progress - Issue #74

---
+++ src/edu/wright/cs/sp16/ceg3120/gui/tabs/QueryBuilderTab.java
@@ -0,0 +1,21 @@
+package edu.wright.cs.sp16.ceg3120.gui.tabs;
+
+import java.awt.Component;
+
+/**
+ * This will be a tab for the query builder under the connection tab.
+ * @author Bonnie
+ *
+ */
+public class QueryBuilderTab extends Component{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Default constructor for the Query Builder tab.
+	 */
+	public QueryBuilderTab() {
+		super();
+		//initComponents();
+	}
+}
